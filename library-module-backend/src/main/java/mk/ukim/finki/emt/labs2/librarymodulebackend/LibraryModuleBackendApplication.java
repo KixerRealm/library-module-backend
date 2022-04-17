2035c6e2bf11a01193df45a851099790b63cd2dd
Kristijan Kostovski <kixer.wade2000@outlook.com>
2022-04-17 18:00:21 +0200

    Backend project created

---
+++ library-module-backend/src/main/java/mk/ukim/finki/emt/labs2/librarymodulebackend/LibraryModuleBackendApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.labs2.librarymodulebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryModuleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryModuleBackendApplication.class, args);
+    }
+
+}
---
+++ library-module-backend/src/test/java/mk/ukim/finki/emt/labs2/librarymodulebackend/LibraryModuleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.labs2.librarymodulebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryModuleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
